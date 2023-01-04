d62056ee753af9cbed540330ce59bc28d0131805
rubelliumm <ahmedrubel935@gmail.com>
2023-01-05 00:14:50 +0600

    updated gitignor

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main',  # add all user defined application main
     'fifawcpredictor',
     'todo',
     'jobapp',
